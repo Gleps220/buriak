f7cd25ce71d406dfdceaf142d6100d86fdfecd96
Gleps220 <bgnet36@gmail.com>
2024-10-23 20:34:34 +0300

    Add files via upload

---
+++ halva.py
@@ -0,0 +1,24 @@
+#pl.figure(dpi=200)
+##x=pl.plot(n,wn80,linewidth=2)
+##y=pl.bar(m1,wn80bnew,color='black')
+##pl.legend([x[0],y[0]],  
+##            ["Теоретическое распределение","Практическое распределение"], 
+##            loc=1) 
+##pl.title('Частота Wn для t = 40')
+##pl.xlabel('Число отсчётов - n')
+##pl.ylabel('Wn')
+###pl.minorticks_on()
+##pl.grid(visible=True,
+##         which='major',
+##         linestyle='--',
+##         linewidth=0.5,
+##         color='0.4') # Формат основных линиий сетки
+##pl.grid(visible=True,
+##         which='minor',
+##         linestyle='--',
+##         linewidth=1,
+##         color='0.4')# Формат побочных линий
+##pl.show()
+
+##pl.xticks(np.arange(0,0.6,step=0.05))
+###pl.yticks(np.arange(0,5,step=0.25))
